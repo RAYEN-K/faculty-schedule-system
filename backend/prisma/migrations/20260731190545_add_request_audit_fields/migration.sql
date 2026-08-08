909db272d80dd43e33b68134922907d4aa619144
rayenelkamel403-hub <rayenelkamel403@gmail.com>
2026-08-08 15:25:13 +0100

    fix: force update for pr

--- backend/prisma/migrations/20260731190545_add_request_audit_fields/migration.sql
+++ backend/prisma/migrations/20260731190545_add_request_audit_fields/migration.sql
@@ -1,3 +1,3 @@
--- AlterTable
+echo "CodeRabbit test update" >> README.md-- AlterTable
 ALTER TABLE "ModificationRequest" ADD COLUMN     "reviewedAt" TIMESTAMP(3),
 ADD COLUMN     "reviewedById" TEXT;
